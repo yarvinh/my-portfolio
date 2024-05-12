f2902a8c9197bd45bc8440faa634d5fc3f857c95
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2024-05-12 19:20:43 -0400

    create ProjectContainer component

---
+++ src/containers/ProjectsContainer.tsx
@@ -0,0 +1,16 @@
+const ProjectsContainer = ()=>{
+    return (
+        <section className="project">
+            <h3>      
+                Work Orders App
+                Jan 2023 - PresentJan 2023 - Present
+            </h3>
+             <p>
+
+                I created this app to help keep buildings maintenance record, you can create an account , login and logout, you can create locations, employees and you can create work orders and assign it to an employees. Backend technologies used: Ruby on rails JWT and postgresql, frontend technologies used: React ,Javascript. 
+            </p>
+        </section>
+    )
+}
+
+export default ProjectsContainer
